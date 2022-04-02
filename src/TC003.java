34c3e2535e70c731d939777f658a62791e9adde5
zgurbanz <98862873+zgurbanz@users.noreply.github.com>
2022-04-02 14:40:43 -0400

    create Test Case 3

---
+++ src/TC003.java
@@ -0,0 +1,14 @@
+public class TC003 {
+    public static void main(String[] args) {
+
+        // Test Case 3
+
+        // writing smoke tests
+
+        // creating a report
+
+        // screenshot in failure
+
+
+    }
+}
