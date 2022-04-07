1abe2a21b6c81587f1f68e546e1f780054ee3017
zgurbanz <98862873+zgurbanz@users.noreply.github.com>
2022-04-06 20:43:45 -0400

    updated TC001

---
+++ src/test_cases/Problem.java
@@ -0,0 +1,12 @@
+package test_cases;
+
+public class Problem {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+
+        }
+
+    }
+}
---
+++ src/test_cases/TC003.java
@@ -0,0 +1,16 @@
+package test_cases;
+
+public class TC003 {
+    public static void main(String[] args) {
+
+        // Test Case 3
+
+        // writing smoke tests
+
+        // creating a report
+
+        // screenshot in failure
+
+
+    }
+}
