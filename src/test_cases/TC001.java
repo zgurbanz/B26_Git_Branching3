b93ce11e37c1142e7bb22f927eb88154d90c00c4
zgurbanz <98862873+zgurbanz@users.noreply.github.com>
2022-04-06 20:12:45 -0400

    updated TC001

---
+++ src/test_cases/TC001.java
@@ -0,0 +1,17 @@
+package test_cases;
+
+public class TC001 {
+    public static void main(String[] args) {
+
+        // 1
+        // 2
+        // 3
+
+        // more useful information
+
+
+
+
+
+    }
+}
